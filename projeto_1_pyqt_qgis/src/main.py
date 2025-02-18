f6cd9a5e3939ac100661c1c3d6b72a25d58d7219
vhenequim <vhenequim@gmail.com>
2025-02-18 10:50:28 -0300

    Added new file with polars

---
+++ projeto_1_pyqt_qgis/src/main.py
@@ -0,0 +1,14 @@
+import polars as pl
+
+
+data = {
+    "nome": ["João", "Maria", "Pedro", "Ana", "Carlos"],
+    "idade": [25, 30, 35, 40, 45],
+    "cidade": ["São Paulo", "Rio de Janeiro", "Brasília", "Salvador", "Fortaleza"]
+}
+
+df = pl.DataFrame(data)
+
+print(df)
+
+
